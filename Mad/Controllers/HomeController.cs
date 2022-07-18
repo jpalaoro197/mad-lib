b06af92d2ac8f23dfe74b352a57538ef4fb74edc
jacobpalaoro <jpalaoro197@gmail.com>
2022-07-18 10:40:58 -0700

    use epicodus c# front end example to build a simple madlib page
    Co-authored-by: Anthony DiFalco <aodifalco@gmail.com>

---
+++ Mad/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Mad.Models;
+namespace Mad.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/mad-lib")]
+    public ActionResult Mad(string name, string adjective, string propernoun, string adjective2, string verb, string verb2, string noun2, string yourname, string verb3)
+    {
+    MadVariable myMadVariable = new MadVariable();
+    myMadVariable.Name = name;
+    myMadVariable.Adjective = adjective;
+    myMadVariable.ProperNoun = propernoun;
+    myMadVariable.Adjective2 = adjective2;
+    myMadVariable.Verb = verb;
+    myMadVariable.Verb2 = verb2;
+    myMadVariable.Noun2 = noun2;
+    myMadVariable.YourName = yourname;
+    myMadVariable.Verb3 = verb3;
+    return View(myMadVariable);
+    }
+  }
+}
---
+++ Mad/Models/MadVariable.cs
@@ -0,0 +1,17 @@
+namespace Mad.Models
+{
+  public class MadVariable
+  {
+    public string Name { get; set; }
+    public string Adjective { get; set; }
+    public string ProperNoun { get; set; }
+    public string Adjective2 { get; set; }
+    public string Verb { get; set; }
+    public string Verb2 { get; set; }
+    public string Noun2 { get; set; }
+    public string Verb3 { get; set; }
+    public string YourName { get; set; }
+
+
+  }
+}
